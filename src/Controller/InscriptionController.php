77845c728270904a3caf658e484dbc1bb74bad2c
Rakotonindrina Pierre Alexandre <rakotonindrinapierre@gmail.com>
2023-02-16 08:56:39 +0300

    il reste que le debug

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -3,19 +3,29 @@ namespace App\Controller;
 
 use App\Entity\Evenement;
 use App\Entity\Inscription;
+use App\Repository\EvenementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Attribute\AsController;
+use Doctrine\Persistence\ManagerRegistry;
 
 #[AsController]
 
 class InscriptionController extends AbstractController{
-    public function __invoke(Inscription $data): Inscription
+    public function __invoke(Inscription $data, EvenementRepository $evenementRepository, ManagerRegistry $doctrine): Inscription
     {
-        echo "here";die;
-        $data->setNom("Pierre");
-        $data->setPrenom("Alexandre");
-        $data->setEmail("mail@mail.mail");
-        $data->setTelephone("123456789");
-        return $data;
+        $entityManager = $doctrine->getManager();
+
+        $evenement = $evenementRepository->find($data->getEvenement());
+        if($evenement->getNombreInscrit()>0) {
+            ///décrément le nombre de personne inscrit jusquà 0
+            $evenement->setNombreInscrit($evenement->getNombreInscrit()-1);
+            $entityManager->persist($evenement);
+            $entityManager->flush();
+            return $data;
+        }
+        else{
+            return new JsonResponse(array("msg"=>"Désolé, le nombre de personne inscrit est déjà complète","erreur"=>true));
+        }
     }
 }
